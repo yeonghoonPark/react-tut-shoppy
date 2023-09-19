488d0f88421881ebef71f17f1eb5627887b7faae
yeonghoon Park <huklmnj2@naver.com>
2023-09-19 22:26:12 +0900

    [feature] created firebase folder and index.ts
     - redefine and export firebaseConfig in index.ts

--- src/App.tsx
+++ src/App.tsx
@@ -10,24 +10,20 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_UR,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-};
+import { firebaseConfig } from "../src/firebase";
+
+import { getDatabase } from "firebase/database";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const database = getDatabase(app);
 
 export default function App() {
   const provider = new GoogleAuthProvider();
   const auth = getAuth();
+
+  console.log(database, "##$$database");
 
   const handleSignIn = () => {
     signInWithPopup(auth, provider)
